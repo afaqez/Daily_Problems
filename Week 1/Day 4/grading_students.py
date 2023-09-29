84e0be68bec92f8af518863b13438bfb139e644a
afaqkhannn <afaqkhan113@gmail.com>
2023-09-30 01:09:54 +0500

    Day 4

---
+++ Week 1/Day 4/grading_students.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    difference = 0
+    remainder = 0
+    for i in range(len(grades)):
+        remainder = grades[i] % 5
+        difference = 5 - remainder
+        if (difference) < 3:
+            if grades[i] > 37:
+                grades[i] += difference
+        
+    return grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
